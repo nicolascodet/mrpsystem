14572f88561b698eed1ac5a55a73d312020cc750
nicolascodet <nicolas.codet5@gmail.com>
2025-02-02 16:58:37 -0800

    Switch to simpler OAuth2 configuration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,31 @@
 import NextAuth from "next-auth";
-import AzureADProvider from "next-auth/providers/azure-ad";
 import type { Session } from "next-auth";
 import type { JWT } from "next-auth/jwt";
 
+const TENANT_ID = "0772737c-2e7f-49c6-a0d6-be3d35bad280";
+
 const handler = NextAuth({
   providers: [
-    AzureADProvider({
+    {
+      id: "azure-ad",
+      name: "Microsoft",
+      type: "oauth",
+      authorization: {
+        url: `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/authorize`,
+        params: { scope: "openid profile email" }
+      },
+      token: `https://login.microsoftonline.com/${TENANT_ID}/oauth2/v2.0/token`,
+      userinfo: "https://graph.microsoft.com/oidc/userinfo",
       clientId: process.env.AZURE_AD_CLIENT_ID!,
       clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
-      tenantId: "0772737c-2e7f-49c6-a0d6-be3d35bad280",
-    }),
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+        }
+      },
+    }
   ],
   callbacks: {
     async jwt({ token, account }) {
